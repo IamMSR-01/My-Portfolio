7d23542e912437af3f36816a022a4eeccd8a174f
Mohd Shaqib Raza <iammsr19413m@gmail.com>
2025-04-01 13:49:20 +0530

    changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ import React, { useRef } from "react";
 import { motion } from "framer-motion";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
-import ResumePDF from "@/assets/resume.pdf";
+import ResumePDF from "@/assets/Resume.pdf";
 
 function RotatingSphere() {
   const sphereRef = useRef();
